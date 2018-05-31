75373ffb84fde1a4aaaf621aef4533389dbf5016
Mario <you@example.com>
2018-05-31 07:33:26 -0600

    Asaber que putas

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',

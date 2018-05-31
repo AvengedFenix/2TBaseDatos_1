21b575492e7a4677e784c8e3626096c3c4b6ba6f
Mario <you@example.com>
2018-05-31 08:10:49 -0600

    Que pex

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var app = express();
- 
+
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(1337)
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: "",
+    password: '',
     database: 'cigarrillos'
 });
 
-connection.connect();
+connection.connect(function (error){
+    if(!!error){
+        console.log('Error');
+    }else{
+        console.log('Connected')
+    }
+})
 
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
     console.log('The solution is: ', results[0].solution);
 });
 
